196e88533a30a0ac2b3724e53a98a0b5542f323b
Iván Paredes Reséndiz <ivanresendiz25@gmail.com>
2020-04-02 21:14:50 -0600

    Update Tacubaya.R

--- Tacubaya.R
+++ Tacubaya.R
@@ -1,4 +1,38 @@
-https://www.r-spatial.org/r/2019/09/26/spatial-networks.html
+#https://www.r-spatial.org/r/2019/09/26/spatial-networks.html
+# We'll use remotes to install packages, install it if needs be:
+if(!"remotes" %in% installed.packages()) {
+  install.packages("remotes")
+}
+
+cran_pkgs = c(
+  "sf",
+  "tidygraph",
+  "igraph",
+  "osmdata",
+  "dplyr",
+  "tibble",
+  "ggplot2",
+  "units",
+  "tmap",
+  "rgrass7",
+  "link2GI",
+  "nabor"
+)
+
+remotes::install_cran(cran_pkgs)
+
+library(sf)
+library(tidygraph)
+library(igraph)
+library(dplyr)
+library(tibble)
+library(ggplot2)
+library(units)
+library(tmap)
+library(osmdata)
+library(rgrass7)
+library(link2GI)
+library(nabor)
 
 muenster <- opq(bbox =  c(7.61, 51.954, 7.636, 51.968)) %>% 
   add_osm_feature(key = 'highway') %>% 
